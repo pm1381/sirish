52415f96616d4a42a35d38fccd1461eab66ffc08
parham minouian <79714647+pm1381@users.noreply.github.com>
2025-12-22 07:51:09 -0800

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+set -x
 set -euo pipefail
 
 echo "▶ Running all Go tests (verbose, race)..."
 
-go test -v -race ./...
-
-echo "✅ All tests passed"
+go test -v -race ./... -coverprofile=coverage.txt -covermode count || exit 1
+go tool cover -func=coverage.txt
+go tool cover -html coverage.txt -o coverage.html
+go get github.com/boumenot/gocover-cobertura
+bash -c "go run github.com/boumenot/gocover-cobertura < coverage.txt > coverage.xml"
